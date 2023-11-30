14960ae06226c853ed49b1c4d0c6ddbe29766f04
Niall Starkey <48092902+TheInquisitiveSpoon@users.noreply.github.com>
2023-11-30 23:11:02 +0000

    Rework on about me section

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function myFunction() {
+    console.log("myFunction called");
     document.getElementById("drp-content").classList.toggle("show");
-    console.log("show added")
 }
 
 window.onclick = function (event) {
   if (!event.target.matches('.drpbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
+      let dropdowns = document.getElementsByClassName("dropdown-content");
+      let i;
 
       console.log(dropdowns.length)
       for (i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
+          let openDropdown = dropdowns[i];
           if (openDropdown.classList.contains('show')) {
               openDropdown.classList.remove('show');
               console.log("show removed");
@@ -19,13 +19,41 @@ window.onclick = function (event) {
   }
 }
 
-var prevScrollpos = window.scrollY;
+let prevScrollpos = window.scrollY;
 window.onscroll = function() {
-  var currentScrollPos = window.scrollY;
-  if (prevScrollpos > currentScrollPos) {
+    let currentScrollPos = window.scrollY;
+    if (prevScrollpos > currentScrollPos) {
     document.getElementById("header").style.top = "0";
   } else {
     document.getElementById("header").style.top = "-80px";
   }
   prevScrollpos = currentScrollPos;
+}
+
+window.onload = function() {
+    openCity(event, 'London')
+    document.getElementById("programming").className += " active";
+};
+function openCity(evt, cityName) {
+    // Declare all variables
+    let i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+        console.log("hidden")
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        console.log("removed active")
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+    console.log("shown active")
 }
